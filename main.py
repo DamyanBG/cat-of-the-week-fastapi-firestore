4151013488b1f30c9114c9ce84aaa4327906d523
DamyanBG <d.dimitrov.1988@abv.bg>
2024-07-09 11:01:20 +0300

    remove cron tasks

--- main.py
+++ main.py
@@ -3,20 +3,11 @@ from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 
 from routers.routes import api_router
-from cron_tasks.create_scheduler import create_scheduler
 
 origins = ["http://localhost", "http://localhost:5173", "http://localhost:3000"]
 
 
-@asynccontextmanager
-async def lifespan(_: FastAPI):
-    scheduler = create_scheduler()
-    scheduler.start()
-    yield
-    scheduler.shutdown(wait=False)
-
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
