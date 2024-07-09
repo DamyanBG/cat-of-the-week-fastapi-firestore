ca2689cd6d17b33de11ff2f4333f2cbc1a3d47b8
DamyanBG <d.dimitrov.1988@abv.bg>
2024-07-09 11:35:14 +0300

    check is image with cat

--- image_recognition/google_vision.py
+++ image_recognition/google_vision.py
@@ -3,18 +3,11 @@ from google.cloud.vision import ImageAnnotatorClient, Image
 from sa import credentials
 
 
-def check_is_safe_image(image_content: bytes) -> bool:
+def check_is_safe_image(image: Image, client: ImageAnnotatorClient) -> bool:
     """Uses the Cloud Vision API to check if an image is safe for work (SFW)."""
-    client = ImageAnnotatorClient(credentials=credentials)
-    image = Image(content=image_content)
 
     response = client.safe_search_detection(image=image)
     safe = response.safe_search_annotation
-
-    # label_response = client.label_detection(image=image)
-    # labels = label_response.label_annotations
-    # for label in labels:
-    #     print(label.description)
 
     # Define your safety thresholds here (adjust as needed)
     likelihood_name = (
@@ -30,3 +23,25 @@ def check_is_safe_image(image_content: bytes) -> bool:
     ] in ["LIKELY", "VERY_LIKELY"]:
         return False
     return True
+
+
+def check_contains_cat(image: Image, client: ImageAnnotatorClient) -> bool:
+    label_response = client.label_detection(image=image)
+    labels = label_response.label_annotations
+    for label in labels:
+        if "cat" in label.description.lower():
+            return True
+
+    return False
+
+
+def check_is_safe_and_contains_cat(image_content: bytes) -> bool:
+    client = ImageAnnotatorClient(credentials=credentials)
+    image = Image(content=image_content)
+
+    is_safe = check_is_safe_image(image, client)
+    contains_cat = check_contains_cat(image, client)
+
+    is_safe_and_contains_cat = is_safe and contains_cat
+
+    return is_safe_and_contains_cat
--- routers/image_route.py
+++ routers/image_route.py
@@ -4,7 +4,7 @@ from queries.image_queries import insert_image
 from utils.image_compression import compress_image_to_webp
 from utils.utils import separate_data_url_from_base64
 from storage.google_cloud_storage import upload_bytes_image, generate_signed_url
-from image_recognition.google_vision import check_is_safe_image
+from image_recognition.google_vision import check_is_safe_and_contains_cat
 from models.image_model import ImageFileName, ImageBase64, ImageWithUrl
 
 images_router = APIRouter(prefix="/images", tags=["images"])
@@ -15,8 +15,8 @@ async def post_image(image: ImageBase64):
     data = image.model_dump()
     base64_data = data["photo_base64"]
     image_bytes = compress_image_to_webp(separate_data_url_from_base64(base64_data)[1])
-    if not check_is_safe_image(image_bytes):
-        raise HTTPException(status_code=400, detail="Image is not safe for work")
+    if not check_is_safe_and_contains_cat(image_bytes):
+        raise HTTPException(status_code=400, detail="Image is not accepted!")
 
     img_file_name_str = upload_bytes_image(image_bytes, ".webp", "image/webp")
     img_file_name = ImageFileName(file_name=img_file_name_str)
